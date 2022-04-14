c5872c4250de3a787684a0080ec5f18a5e998d3f
ivelco <ivelin_conchev@abv.bg>
2022-04-14 09:16:46 +0300

    Add positions controller

---
+++ Football/Controllers/PositionsController.cs
@@ -0,0 +1,44 @@
+﻿namespace Football.Controllers
+{
+    using Football.Core.Constants;
+    using Football.Infrastructure.Data;
+    using Football.Infrastructure.Data.Models;
+    using Football.Models.Positions;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PositionsController : Controller
+    {
+        private readonly FootballDbContext data;
+
+        public PositionsController(FootballDbContext _data)
+        {
+            this.data = _data;
+        }
+
+        public IActionResult Add(AddPositionFormModel position)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(position);
+            }
+
+            var positionData = new Position
+            {
+                Name = position.Name,
+            };
+
+            this.data.Positions.Add(positionData);
+
+            this.data.SaveChanges();
+
+            ViewData[MessageConstant.SuccessMessage] = "Успешен запис";
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Football/Models/Positions/AddPositionFormModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Football.Models.Positions
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Football.Infrastructure.Data.DataConstants.Position;
+
+    public class AddPositionFormModel
+    {
+        [Required]
+        [StringLength(PositionNameMaxLength)]
+        public string? Name { get; set; }
+    }
+}
