ac537e6e37bd8015833e5d2787135beac7818e8e
ivelco <ivelin_conchev@abv.bg>
2022-04-22 19:00:17 +0300

    Add statiscs with js

---
+++ Football/Controllers/Api/StatisticsApiController.cs
@@ -0,0 +1,32 @@
+﻿namespace Football.Controllers.Api
+{
+    using Football.Infrastructure.Data;
+    using Football.Models.Api.Statistics;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly FootballDbContext data;
+
+        public StatisticsApiController(FootballDbContext data)
+            => this.data = data;
+
+        [HttpGet]
+        public StatisticsResponseModel GetStatistics()
+        {
+            var totalTeams = this.data.Teams.Count();
+            var totalUsers = this.data.Users.Count();
+
+
+            return new StatisticsResponseModel
+            {
+                TotalTeams = totalTeams,
+                TotalUsers = totalUsers,
+                SumTeams = 0
+            };
+        }
+    }
+}
